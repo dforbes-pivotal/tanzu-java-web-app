18388261b6107e15076918b6285bccf27a3a103f
Donald <42580718+dforbes-pivotal@users.noreply.github.com>
2022-08-10 15:49:28 +0100

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from TAP!", controller.index());
+        assertEquals("Greetings from Tanzu Application Platform based on spring.", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from TAP!"));
+            .andExpect(content().string("Greetings from Tanzu Application Platform based on spring."));
     }
 }
