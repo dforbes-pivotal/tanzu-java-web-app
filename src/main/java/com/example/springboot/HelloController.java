a94bef2c7405631f34f05b907b84d6968ee7b5e4
Donald <42580718+dforbes-pivotal@users.noreply.github.com>
2022-08-11 15:53:45 +0100

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Tanzu Application Platform based on spring.";
+		return "Greetings from Tanzu Application Platform based on Spring.";
 	}
 
 }
