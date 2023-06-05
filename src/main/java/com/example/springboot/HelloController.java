283fcd4201e9cbca693c2765d96893c9110a9a5c
Donald <42580718+dforbes-pivotal@users.noreply.github.com>
2023-06-05 14:55:28 +0100

    Change title to black

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
 	@RequestMapping("live-update")
 	public String update() {
 		return "<html><body>" + 
-		          "<h1 style=\"color:orange;\">Greetings from Tanzu Application Platform</h1>" + 
+		          "<h1 style=\"color:black;\">Greetings from Tanzu Application Platform</h1>" + 
 				  "<br><br>" +
 				  "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>" +
 				  "</body></html>";
